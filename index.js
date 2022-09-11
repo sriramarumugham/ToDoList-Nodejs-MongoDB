0a5936fccfe035d2d9e0f85107dc06ebb1c9ff35
Sreeram A <sriramlibra0@gmail.com>
2022-09-11 09:27:45 +0530

    ui design

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const { render } = require('ejs');
 const express=require('express');
 
 const app=express();
 
 const port =8000;
+
+
+//setting view engine;
+app.set('view engine' , 'ejs');
+
+app.set('views' , render('./views'))
+
+//setting up the middleware;
+
+app.use('/' , require('./routes/index'))
+
+//conntecting asserts
+
+app.use(express.static("asserts"));
+
 
 app.listen(port , function (err){
 
